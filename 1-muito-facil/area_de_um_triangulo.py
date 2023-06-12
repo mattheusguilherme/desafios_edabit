6b19b41d5c099e7d2399f602c38e3cc2c1c29d87
Matheus Guilherme <mgsmmanhaes0209@gmail.com>
2023-06-12 14:11:40 -0300

    Create area_de_um_triangulo.py

---
+++ 1-muito-facil/area_de_um_triangulo.py
@@ -0,0 +1,5 @@
+#Escreva uma função que pegue a base e a altura de um triângulo e retorne sua área.
+#A área de um triângulo é: (base * altura) / 2
+
+def tri_area(base, height):
+	return base * height * .5
