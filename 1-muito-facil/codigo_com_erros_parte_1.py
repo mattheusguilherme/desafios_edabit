745cb16829ff8f7d3c4b1a36c4161a37be94b66c
Matheus Guilherme <mgsmmanhaes0209@gmail.com>
2023-06-12 14:36:32 -0300

    Create codigo_com_erros_parte_1.py

---
+++ 1-muito-facil/codigo_com_erros_parte_1.py
@@ -0,0 +1,6 @@
+#Corrija o código na guia de código para passar neste desafio (somente erros de sintaxe). Veja os exemplos abaixo para ter uma ideia do que a função deve fazer.
+#LEIA CADA PALAVRA CUIDADOSAMENTE, CARACTER POR CARACTER!
+#Não pense demais nesse desafio; não é para ser difícil.
+
+def cubes(a):
+  return a ** 3
